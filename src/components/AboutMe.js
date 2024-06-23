321ab7dfa42908dc6fa395b123518ee3893f66f4
Ankur Maurya <Ankur.Maurya@Cevalogistics.com>
2024-06-23 10:01:56 +0530

    Made some font changes

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,8 +5,7 @@ const AboutMe = () => {
   return (
     <div className="about-me">
       <h1>About Me</h1>
-      <p>This is a sample project to demonstrate a React application with routing and API integration.</p>
-      <p>Author: Your Name</p>
+      <p>Author: Ankur Maurya</p>
     </div>
   );
 };
